2dc1c3156d3c4986dfe791aba195fa34094310b1
ydallelll-create <ydallelll@gmail.com>
2026-08-11 01:01:52 +0200

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://doblobisgpditfwgycgg.supabase.co/rest/v1/";
+const SUPABASE_KEY = "sb_publishable_Ekh__PoeQ8S4RiV1H9K8eg_7puRFR_A";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
